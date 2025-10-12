a4c9921f1faa29c4108fd99e4c19a4587499408d
fuivito <vittorio.damioli@gmail.com>
2025-10-12 21:20:37 +0100

    adding clients endpoint

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import health#, clients#,agreements, dashboard, policies, proposals
+from routers import health, clients#,agreements, dashboard, policies, proposals
 
 app = FastAPI(
     title="LendInsure API",
@@ -24,7 +24,7 @@ def root():
 # Register routers
 app.include_router(health.router)
 # app.include_router(agreements.router)
-# app.include_router(clients.router)
+app.include_router(clients.router)
 # app.include_router(dashboard.router)
 # app.include_router(policies.router)
 # app.include_router(proposals.router)
